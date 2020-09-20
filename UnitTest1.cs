8520df2e272b5b691c7e2c2cc91f20a32989a55c
Забозлаев Илья Вячеславович <zabozlaeviv@sovcombank.ru>
2020-09-20 15:24:30 +0300

    testing

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace dotnet_actions
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
